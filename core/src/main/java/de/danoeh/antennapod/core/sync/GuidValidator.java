4e0da6ea754e9137b6868a95bedac53e1ded7b27
Karev <supersrt1@gmail.com>
2022-02-10 14:28:45 -0500

    2.4.0

--- core/src/main/java/de/danoeh/antennapod/core/sync/GuidValidator.java
+++ core/src/main/java/de/danoeh/antennapod/core/sync/GuidValidator.java
@@ -4,8 +4,7 @@ public class GuidValidator {
 
     public static boolean isValidGuid(String guid) {
         return guid != null
-                && !guid.trim().isEmpty()
-                && !guid.equals("null");
+                && !guid.trim().isEmpty();
     }
 }
 
